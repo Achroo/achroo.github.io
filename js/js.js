da646bb23cb0e59941f8e06c4e9eca4029f9c478
Achro <49365088+Achroo@users.noreply.github.com>
2021-01-24 15:43:01 +0100

    Portfolio Initial Ver
    Initial version for my portfolio. Version 1.0.

---
+++ js/js.js
@@ -0,0 +1,13 @@
+function steamButtonClick() {
+    window.location.href = "https://steamcommunity.com/id/achroxd/";
+}
+
+function githubButtonClick() {
+    window.location.href = "https://github.com/Achroo";
+}
+
+function twitterButtonClick() {
+    window.location.href = "https://twitter.com/angelpcor";
+}
+
+document.getElementsByClassName('gmod-gif').play();
